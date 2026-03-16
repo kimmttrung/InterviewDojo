e920cfe74e2f2f4a3a5bad8b5e504557ed31fd8e
dinhvanit <nguyendinhvanefto@gmail.com>
2026-03-16 22:43:25 +0700

    feat: configure swagger ui at /api/docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //set prefix api
+  app.setGlobalPrefix('api/v1');
+
+  // cau hinh validation global
+  app.useGlobalPipes(new ValidationPipe());
+
+  // cau hinh swagger
+  const config = new DocumentBuilder()
+    .setTitle('InterviewDojo API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth() // ho tro test token
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
+  console.log('Swagger UI: http://localhost:3000/api/docs');
 }
 bootstrap();
